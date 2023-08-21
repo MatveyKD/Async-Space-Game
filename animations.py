f2688c7154f3953034c87f8efa4c308259a71090
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2023-08-21 14:12:10 +0300

    Add year, phrases and pace of game

--- animations.py
+++ animations.py
@@ -7,7 +7,7 @@ import asyncio
 
 from physics import update_speed
 from tools import draw_frame, get_frame_size
-from obstacles import Obstacle, show_obstacles
+from obstacles import Obstacle
 from explosion import explode
 
 
@@ -21,6 +21,18 @@ COROUTINES = []
 OBSTACLES = []
 obstacles_in_last_collisions = []
 
+PHRASES = {
+    1957: "First Sputnik",
+    1961: "Gagarin flew!",
+    1969: "Armstrong got on the moon!",
+    1971: "First orbital space station Salute-1",
+    1977: "Voyager-1 and Voyager-2 flew",
+    1981: "Flight of the Shuttle Columbia",
+    1998: 'ISS start building',
+    2011: 'Voyager-1 has left the Solar System',
+    2020: "Take the plasma gun! Shoot the garbage!",
+}
+
 GAMEOVER_TEXT = """
    _____                         ____                 
   / ____|                       / __ \                
@@ -31,6 +43,9 @@ GAMEOVER_TEXT = """
                                                       
                                                       
 """
+
+year = 1957
+cur_phrase = None
 
 
 def read_controls(canvas):
@@ -91,6 +106,30 @@ async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
     OBSTACLES.remove(obstacle)
 
 
+def get_garbage_delay_tics(year):
+    if year < 1961:
+        return None
+    elif year < 1969:
+        return 20
+    elif year < 1981:
+        return 14
+    elif year < 1995:
+        return 10
+    elif year < 2010:
+        return 8
+    elif year < 2020:
+        return 6
+    else:
+        return 2
+
+
+def update_cur_phrase():
+    global cur_phrase
+    phrase = PHRASES.get(int(year))
+    print(phrase)
+    cur_phrase = phrase if phrase else cur_phrase
+
+
 def load_frames(dir_name):
     frames = []
     for frame_path in os.listdir(f"frames/{dir_name}"):
@@ -125,17 +164,24 @@ async def show_gameover(canvas):
 
 async def fill_orbit_with_garbage(canvas, height, frames):
     while True:
-        column = random.randint(1, height - 1)
-        frame = random.choice(frames)
-        COROUTINES.append(fly_garbage(canvas, column, frame))
-        await sleep(random.randint(3, 20))
+        garbage_delay_tics = get_garbage_delay_tics(year)
+        if garbage_delay_tics:
+            column = random.randint(1, height - 1)
+            frame = random.choice(frames)
+            COROUTINES.append(fly_garbage(canvas, column, frame))
+            await sleep(garbage_delay_tics)
+        await sleep()
 
 
 def draw(canvas):
+    global year
     curses.curs_set(False)
     canvas.nodelay(True)
     symbols = "+*.:'"
     width, height = canvas.getmaxyx()
+    year_row, year_column = width-3, height-10
+    phrase_row, phrase_column = width-6, height-40
+    update_cur_phrase()
     spaceship_frames = load_frames("spaceship")
     garbage_frames = load_frames("garbage")
     stars = 100
@@ -144,7 +190,6 @@ def draw(canvas):
         COROUTINES.append(blink(canvas, row, column, random.choice(symbols)))
     COROUTINES.append(animate_spaceship(canvas, width//2, height//2, spaceship_frames))
     COROUTINES.append(fill_orbit_with_garbage(canvas, height, garbage_frames))
-    COROUTINES.append(show_obstacles(canvas, OBSTACLES))
 
     while True:
         for coroutine in COROUTINES.copy():
@@ -153,8 +198,13 @@ def draw(canvas):
             except StopIteration:
                 COROUTINES.remove(coroutine)
         time.sleep(0.1)
+        canvas.addstr(year_row, year_column, str(int(year)))
+        draw_frame(canvas, phrase_row, phrase_column, cur_phrase, negative=True)
+        update_cur_phrase()
+        draw_frame(canvas, phrase_row, phrase_column, cur_phrase)
         canvas.refresh()
         canvas.border()
+        year += 0.1
 
 
 async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
@@ -211,13 +261,14 @@ async def animate_spaceship(canvas, row, column, frames):
         column = max(0, column)
         column = min(column + columns, window_size[1]) - columns
 
-        if space_pressed:
-            fires.append(fire(canvas, row, column))
-        for coroutine in fires:
-            try:
-                coroutine.send(None)
-            except StopIteration:
-                fires.remove(coroutine)
+        if year >= 2020:
+            if space_pressed:
+                fires.append(fire(canvas, row, column))
+            for coroutine in fires:
+                try:
+                    coroutine.send(None)
+                except StopIteration:
+                    fires.remove(coroutine)
 
         for obstacle in OBSTACLES:
             if obstacle.has_collision(row, column):
